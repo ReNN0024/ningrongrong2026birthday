1cba3a74dab271d70184facb2dd92bcc138a8e17
user8006203432 <4368779786075050-user8006203432@noreply.coze.cn>
2026-08-26 18:36:48 +0800

    feat: 调整碎片位置 - 暝夜移至4_3，莲花去国一千年移至5_1，知晓我在的人移至2_2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
   const isIOS = /iP(ad|hone|od)/.test(navigator.userAgent) || (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
 
   const placeholderColors = ["#caa7a2", "#cdbb83", "#9bb9bd", "#aabe91", "#c494aa", "#b7a58e", "#d5aa7c"];
-  const officialNames = ["致绽放的你", "夜", "心动瞬间", "冲调午后", "槐花冰奶七分糖", "", "海落潮升", "涌流幻梦之蝶", "引梦渡海", "伴生", "锋芒", "拙习", "八音", "左满舵", "莲花去国一千年", "赴明日如赴前尘", "知晓我在的人"];
+  const officialNames = ["致绽放的你", "", "心动瞬间", "冲调午后", "槐花冰奶七分糖", "知晓我在的人", "海落潮升", "涌流幻梦之蝶", "引梦渡海", "伴生", "锋芒", "拙习", "八音", "左满舵", "夜", "赴明日如赴前尘", "莲花去国一千年"];
   const formatFragmentName = name => name.length > 7 ? escapeHTML(name.slice(0, 4)) + '<br>' + escapeHTML(name.slice(4)) : escapeHTML(name);
   const slotForIndex = index => `${Math.floor(index / 4) + 1}_${index % 4 + 1}`;
   const logos = Array.from({ length: 44 }, (_, index) => {
