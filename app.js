891aa661a23e307d2ca42e1b967d5cf52b50a9a4
user8006203432 <4368779786075050-user8006203432@noreply.coze.cn>
2026-08-26 18:52:01 +0800

    feat: 调整碎片位置 - 冲调午后移至3_1，左满舵移至3_2，槐花冰奶七分糖移至3_3，夜移至4_2，被挤占碎片移至11_1/11_2/11_3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
   const isIOS = /iP(ad|hone|od)/.test(navigator.userAgent) || (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
 
   const placeholderColors = ["#caa7a2", "#cdbb83", "#9bb9bd", "#aabe91", "#c494aa", "#b7a58e", "#d5aa7c"];
-  const officialNames = ["致绽放的你", "", "赴明日如赴前尘", "涌流幻梦之蝶", "锋芒", "知晓我在的人", "海落潮升", "", "引梦渡海", "伴生", "", "拙习", "八音", "左满舵", "夜", "", "莲花去国一千年", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "槐花冰奶七分糖", "冲调午后", "心动瞬间"];
+  const officialNames = ["致绽放的你", "", "赴明日如赴前尘", "涌流幻梦之蝶", "锋芒", "知晓我在的人", "", "", "冲调午后", "左满舵", "槐花冰奶七分糖", "拙习", "八音", "暝夜", "", "", "莲花去国一千年", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "伴生", "引梦渡海", "海落潮升", "心动瞬间"];
   const formatFragmentName = name => name.length > 7 ? escapeHTML(name.slice(0, 4)) + '<br>' + escapeHTML(name.slice(4)) : escapeHTML(name);
   const slotForIndex = index => `${Math.floor(index / 4) + 1}_${index % 4 + 1}`;
   const logos = Array.from({ length: 44 }, (_, index) => {
